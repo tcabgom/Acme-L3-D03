d8638aa33fce92c1e2fc3823d2a69e6c26f35fd6
tcabgom <tcabgom222@gmail.com>
2023-04-13 18:25:24 +0200

    Updated repository offer filter

--- src/main/java/acme/features/authenticated/offer/AuthenticatedOfferRepository.java
+++ src/main/java/acme/features/authenticated/offer/AuthenticatedOfferRepository.java
@@ -12,7 +12,7 @@ import acme.framework.repositories.AbstractRepository;
 @Repository
 public interface AuthenticatedOfferRepository extends AbstractRepository {
 
-	@Query("select o from Offer o WHERE o.availabilityPeriodEnd > CURRENT_DATE")
+	@Query("select o from Offer o WHERE (o.availabilityPeriodEnd > CURRENT_DATE and o.availabilityPeriodStart < CURRENT_DATE)")
 	Collection<Offer> findActiveOffers();
 
 	@Query("select o from Offer o where o.id = :id")
